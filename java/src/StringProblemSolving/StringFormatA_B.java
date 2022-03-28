0861a1e4bd316e0bdd6de8bad3ff8e515a9fb2db
Ayush Agrawal <ayushagrawal@Ayushs-MacBook-Air.local>
2022-03-28 22:39:53 +0530

    Ayush|Sabya String Consecutive And Format

--- java/src/StringProblemSolving/StringFormatA_B.java
+++ java/src/StringProblemSolving/StringFormatA_B.java
@@ -1,11 +1,26 @@
 package StringProblemSolving;
-
+/*
+This program is for checking consecutive A and B and return the count to get the exact
+number of character count and formatted string needs to be deleted to achieve formatted string i.e. A..AB..B
+ */
 
 public class StringFormatA_B {
     public void ReplecateA_B() {
-        String s = "BAAABAB";
+        StringBuilder sb = new StringBuilder();
+        String s = "AAABAB";
+        sb.append(s);
         int count = 0;
-        char[] ch = s.toCharArray();
-
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'B') {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(j) == 'A') {
+                        count++;
+                        sb.deleteCharAt(j);
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("Count of character that needs to be deleted is " + count + " " + "and formatted final string is " + sb);
     }
 }
